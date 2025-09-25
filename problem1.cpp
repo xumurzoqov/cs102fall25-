a3d1e007a1851697f53dd39194994dd3d9a4ec71
Xudayberdi Umurzoqov <x.umurzoqov@newuzbekistanuniversity.uz>
2025-09-25 10:21:33 +0500

    this is second week practice

--- problem1.cpp
+++ problem1.cpp
@@ -1,7 +1,16 @@
-  #include <iostream>
+#include <iostream>
+using namespace std;
 
-  int main() {
-      std::cout << "Hello, NUU!";
-      return 0;
+int main() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    if (x >= 0) {
+        cout << " positive." << endl;
+    } else {
+        cout << " negative." << endl;
+    }
+
+    return 0;
 }
-
